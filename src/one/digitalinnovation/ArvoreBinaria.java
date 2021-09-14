c161f4df18fab6798a4948c0229729f44afd6635
Paulo Becker <beckerpaulo81@gmail.com>
2021-09-14 14:33:52 -0300

    Utilização da classe ArvoreBinaria

--- src/one/digitalinnovation/ArvoreBinaria.java
+++ src/one/digitalinnovation/ArvoreBinaria.java
@@ -30,6 +30,19 @@ public class ArvoreBinaria<T extends Comparable<T>> {
 		}
 		return atual;
 	}
+	
+	 public void exibirInOrdem(){
+	        System.out.println("\n Exibindo InOrdem");
+	        exibirInOrdem(this.raiz);
+	    }
+
+	    private void exibirInOrdem(BinNo<T> atual){
+	        if(atual != null){
+	            exibirInOrdem(atual.getNoEsq());
+	            System.out.print(atual.getConteudo() + ", ");
+	            exibirInOrdem(atual.getNoDir());
+	        }
+	    }
 
 	public void exibirPosOrdem() {
 		System.out.println("\n Exibindo PosOrdem");
